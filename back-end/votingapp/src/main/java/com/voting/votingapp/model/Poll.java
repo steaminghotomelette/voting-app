f6cbeadcc123fd0ac89840dbd5598964289b13a4
steaminghotomelette <steaminghotomelette@proton.me>
2025-04-24 01:32:37 +0800

    Created model package for entities and added poll, options and votes entities

---
+++ back-end/votingapp/src/main/java/com/voting/votingapp/model/Poll.java
@@ -0,0 +1,29 @@
+package com.voting.votingapp.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    @ElementCollection // This creates a new entity (table)
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection // Counters can be denormalized in the future
+    private List<Long> votes = new ArrayList<>();
+}
